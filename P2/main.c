1e5a7aa71378402526c50bd63a557497aecf40ab
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-11-27 08:51:25 -0300

    ....

--- P2/main.c
+++ P2/main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <regex.h>
 #include "./TokenType.h"
-
-#define SIZEOF(a) sizeof(a) / sizeof(*a)
+#include "./utils.c"
+// #include "./analLex.c"
 
 // Panic Mode
 /*
@@ -21,6 +20,21 @@ For each line of code
 
 // Mock code lines
 char codeLines[] = "read x\ny:=10\nz:=x+y";
+tokenType tokens[15];
+
+void readFile(char *fileInput);
+void GLC(tokenType *tokens, size_t length);
+
+int main(int argc, char const *argv[])
+{
+
+    // Reading the file
+    readFile("./teste.txt");
+
+    GLC(tokens, SIZEOF(tokens));
+
+    return 0;
+}
 
 // Read the file to analyze
 void readFile(char *fileInput)
@@ -30,7 +44,6 @@ void readFile(char *fileInput)
     int countLines = 0;
     char line[100];
     char *token;
-    tokenType tokens[10];
 
     if (file == NULL)
     {
@@ -38,44 +51,159 @@ void readFile(char *fileInput)
     }
     else
     {
+
         int i = 0;
         while (fgets(line, sizeof(line), file) != NULL)
         {
+            countLines++;
+
             token = strtok(line, " ");
 
             while (token != NULL)
             {
                 token[strcspn(token, "\n")] = '\0';
-                const int type = getTokenType(token);
-                if (type != INVALID_TOKEN)
-                {
-                    tokens[i].token = strdup(token);
-                    tokens[i].type = type;
-                }
+                TokenType type = getTokenType(token);
+                tokens[i].token = strdup(token);
+                tokens[i].type = type;
+                tokens[i].row = countLines;
                 token = strtok(NULL, " ");
                 i++;
             }
-
-            countLines++;
         }
     }
 
     fclose(file);
 
-    printf("\n> Number of lines: %d\n", countLines);
-    for (size_t i = 0; i < SIZEOF(tokens); i++)
+    // printf("\n> Number of lines: %d\n", countLines);
+    /* for (size_t i = 0; i < SIZEOF(tokens); i++)
     {
-        printf("%s->%d\n", tokens[i].token, tokens[i].type);
+        printf("%s -> %d\n", tokens[i].token, tokens[i].type);
 
         if (&tokens[i].type == END_OF_FILE)
             break;
-    }
+    } */
 }
 
-int main(int argc, char const *argv[])
+void GLC(tokenType *tokens, size_t length)
 {
-    // Reading the file
-    readFile("./teste.txt");
-
-    return 0;
+    for (int i = 1; i <= length; i++)
+    {
+        printf("%s -> %d\n", tokens[i - 1].token, tokens[i - 1].type);
+        printf("%s -> %d\n", tokens[i].token, tokens[i].type);
+        // if (tokens[i].type == NULL)
+        //     break;
+        if (tokens[i - 1].type == INVALID_TOKEN)
+        {
+            printf("\nERROR: Invalid token: '%s'\n", tokens[i - 1].token);
+            printf("LINE: %d\n", tokens[i - 1].row);
+        }
+        // <read> → read identifier
+        else if (tokens[i - 1].type == READ)
+        {
+            if (tokens[i - 1].row != tokens[i].row)
+            {
+                printf("\n%s -> ERROR\n", tokens[i - 1].token);
+            }
+            else if (tokens[i].type == VAR)
+            {
+                printf("\n%s -> MATCH\n", tokens[i - 1].token);
+                printf("%s -> MATCH\n", tokens[i].token);
+            }
+            else
+            {
+                printf("\nERROR: Expected VAR after '%s'\n", tokens[i - 1].token);
+                printf("LINE: %d\n", tokens[i - 1].row);
+            }
+        }
+        // <write> → write identifier
+        else if (tokens[i - 1].type == WRITE)
+        {
+            if (tokens[i - 1].row != tokens[i].row)
+            {
+                printf("\n%s -> ERROR\n", tokens[i - 1].token);
+            }
+            else if (tokens[i].type == VAR)
+            {
+                printf("\n%s -> MATCH\n", tokens[i - 1].token);
+                printf("%s -> MATCH\n", tokens[i].token);
+            }
+            else
+            {
+                printf("\nERROR: Expected VAR after '%s'\n", tokens[i - 1].token);
+                printf("LINE: %d\n", tokens[i - 1].row);
+            }
+        }
+        // <assign> → identifier := <arithmetic exp>
+        /* else if (tokens[i].type == ASSIGN)
+        {
+            if (tokens[i - 1].row != tokens[i].row)
+            {
+                printf("\n%s -> ERROR\n", tokens[i - 1].token);
+            }
+            else if (tokens[i].type == VAR || tokens[i].type == NUM)
+            {
+                if (tokens[i].row != tokens[i + 1].row)
+                {
+                    printf("\n%s -> ERROR\n", tokens[i - 1].token);
+                }
+                else if (tokens[i + 1].type == ASSIGN)
+                {
+                }
+            }
+        } */
+        // <arithmetic exp> → <operating> {<arithmetic op> <operating>}
+        else if (tokens[i - 1].type == NUM || tokens[i - 1].type == VAR)
+        {
+            if (tokens[i - 1].row != tokens[i].row)
+            {
+                printf("\n%s -> ERROR\n", tokens[i - 1].token);
+            }
+            else if (tokens[i].type == ADD || tokens[i].type == SUB || tokens[i].type == MUL || tokens[i].type == DIV)
+            {
+                if (tokens[i].row != tokens[i + 1].row)
+                {
+                    printf("\n%s -> ERROR\n", tokens[i].token);
+                }
+                else if (tokens[i + 1].type == NUM || tokens[i + 1].type == VAR)
+                {
+                    printf("\n%s -> MATCH\n", tokens[i - 1].token);
+                    printf("%s -> MATCH\n", tokens[i].token);
+                    printf("%s -> MATCH\n", tokens[i + 1].token);
+                }
+                else
+                {
+                    printf("\nERROR: Expected NUM or VAR after '%s'\n", tokens[i].token);
+                    printf("LINE: %d\n", tokens[i].row);
+                }
+            }
+            else
+            {
+                printf("\nERROR: Expected '+', '-', '*' or '/' after '%s'\n", tokens[i - 1].token);
+                printf("LINE: %d\n", tokens[i - 1].row);
+            }
+        }
+        // <operating> → identifier | num
+        else if (tokens[i - 1].type == NUM || tokens[i - 1].type == VAR)
+        {
+            printf("\n%s -> MATCH\n", tokens[i - 1].token);
+        }
+        // <arithmetic op> → + | *
+        else if (tokens[i - 1].type == ADD || tokens[i - 1].type == MUL)
+        {
+            if (tokens[i - 1].row != tokens[i].row)
+            {
+                printf("\n%s -> ERROR\n", tokens[i - 1].token);
+            }
+            else if (tokens[i].type == NUM || tokens[i].type == VAR)
+            {
+                printf("\n%s -> MATCH\n", tokens[i - 1].token);
+                printf("%s -> MATCH\n", tokens[i].token);
+            }
+            else
+            {
+                printf("\nERROR: Expected NUM or VAR after '%s'\n", tokens[i - 1].token);
+                printf("LINE: %d\n", tokens[i - 1].row);
+            }
+        }
+    }
 }
---
+++ P2/utils.c
@@ -0,0 +1,1 @@
+#define SIZEOF(a) sizeof(a) / sizeof(*a)
